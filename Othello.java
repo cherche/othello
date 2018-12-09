c960923b5794f29dfb759b4bff838e2032a5792c
Ryan Nguyen <contactryannguyen@gmail.com>
2018-12-09 17:11:35 -0500

    Made game playable in command line

--- Othello.java
+++ Othello.java
@@ -8,6 +8,14 @@ import java.util.*;
  */
 public class Othello {
   /**
+   * The turn number
+   */
+  private int turn = 1;
+  /**
+   * The number of players
+   */
+  private int playerCount = 3;
+  /**
    * The width of the board
    */
   private int width;
@@ -20,7 +28,6 @@ public class Othello {
    */
   private int[][] board;
 
-  /*
   public String boardToString() {
     String string = "";
 
@@ -39,15 +46,37 @@ public class Othello {
     return "(" + pos[0] + ", " + pos[1] + ")";
   }
 
+  public void move(int x, int y) {
+    State state = place(new int[] {x, y});
+    System.out.println(state.toString());
+    System.out.println(boardToString());
+  }
+
   public static void main(String[] args) {
     Othello othello = new Othello(8, 8);
     System.out.println(othello.boardToString());
-    othello.place(new int[] {4, 2}, 1);
-    System.out.println(othello.boardToString());
+
+    while (true) {
+      System.out.println("# Player " + othello.getTurn());
+      int x = IBIO.inputInt("x: ");
+      int y = IBIO.inputInt("y: ");
+      othello.move(x, y);
+    }
   }
-  */
 
   public Othello(int width, int height) {
+    this.width = width;
+    this.height = height;
+    this.board = new int[width][height];
+    // This is just something rough to try to initialize the board
+    int cWidth = width / 2 - 1;
+    int cHeight = height / 2 - 1;
+    board[cWidth][cHeight] = board[cWidth + 1][cHeight + 1] = 1;
+    board[cWidth + 1][cHeight] = board[cWidth][cHeight + 1] = 2;
+  }
+
+  public Othello(int width, int height, int playerCount) {
+    this.playerCount = playerCount;
     this.width = width;
     this.height = height;
     this.board = new int[width][height];
@@ -62,35 +91,54 @@ public class Othello {
    * Places a tile on the board with a check for legality
    *
    * @param pos the position where a tile is to be placed
-   * @param attacker the id of the placed tile
    * @return all tiles that would be captured if this move were made
    */
-  public ArrayList<int[]> place(int[] pos, int attacker) {
-    ArrayList<int[]> captures = getCaptures(pos, attacker);
+  public State place(int[] pos) {
+    State state = new State();
 
-    // Basically,if you didn't capture anything, it won't place the tile
+    if (!hasValidMoves()) {
+      state.isSkipped = true;
+      increaseTurn();
+    // Basically, if you didn't capture anything, it won't place the tile
     // Users will have to check for this themselves with the return value
-    if (captures.size() > 0) {
-      setBoardValue(pos, attacker);
+    // Also, you obviously need to place a tile in an empty space
+    } else if (isValidMove(pos, turn)) {
+      ArrayList<int[]> updates = getCaptures(pos, turn);
+      updates.add(pos);
 
-      for (int i = 0; i < captures.size(); i++) {
-        int[] capture = captures.get(i);
-        setBoardValue(capture, attacker);
+      for (int i = 0; i < updates.size(); i++) {
+        int[] update = updates.get(i);
+        setBoardValue(update, turn);
       }
+
+      // If the move was valid, the next player is allowed to move
+      increaseTurn();
+
+      state.updates = new ArrayList<int[]>(updates);
+      state.isValidMove = true;
     }
 
-    return captures;
+    // Returning the coordinates of the captured tiles makes
+    // rendering a lot easier for the user since they know
+    // exactly which tiles must be updated
+    return state;
   }
 
   /**
    * Gets all tiles that would be captured
    *
    * @param start the position where a tile would be placed
-   * @param attacker the id of the placed tile
+   * @param attacker the attacker
    * @return all tiles that would be captured if this move were made
    */
   public ArrayList<int[]> getCaptures(int[] start, int attacker) {
     ArrayList<int[]> captures = new ArrayList<int[]>();
+
+    // If the starting tile is not on the board,
+    // the move was clearly invalid and no pieces would be captured
+    if (!isInBoard(start)) {
+      return captures;
+    }
 
     // We'll use these to search for captured stones
     int[][] directions = {
@@ -107,17 +155,17 @@ public class Othello {
     for (int i = 0; i < directions.length; i++) {
       ArrayList<int[]> temp = new ArrayList<int[]>();
       int[] dir = directions[i];
-      // Walk in that direction
       int dx = dir[0];
       int dy = dir[1];
-      int[] pos = {start[0], start[1]};
+      // We clone so as not to mutate start for future iterations
+      int[] pos = start.clone();
       // System.out.println("Trying " + format(dir));
 
       // I thought about this for a long time and couldn't
       // come up with a nice stopping position
       // How sad
       while (true) {
-        // Try walking in that direction
+        // Walk in that direction
         pos[0] += dx;
         pos[1] += dy;
 
@@ -147,6 +195,15 @@ public class Othello {
     }
 
     return captures;
+  }
+
+  /**
+   * Gets the turn number
+   *
+   * @return the turn
+   */
+  public int getTurn() {
+    return turn;
   }
 
   /*
@@ -194,7 +251,7 @@ public class Othello {
    * @param pos a position on the board
    * @return the value of the board at that position
    */
-  private int getBoardValue(int[] pos) {
+  public int getBoardValue(int[] pos) {
     int x = pos[0];
     int y = pos[1];
 
@@ -212,5 +269,84 @@ public class Othello {
     int y = pos[1];
 
     board[x][y] = val;
+  }
+
+  /**
+   * Determines whether a move is valid
+   *
+   * @param pos a position on the board
+   * @param attacker the attacker
+   * @return whether or not a move is valid
+   */
+  private boolean isValidMove(int[] pos, int attacker) {
+    // Technically, this may be optimized since getCaptures
+    // doesn't stop as soon as it finds a valid move
+    return getCaptures(pos, attacker).size() > 0;
+  }
+
+  /**
+   * Determines whether the current player has a valid move
+   *
+   * @return whether or not there is valid move for the current player
+   */
+  private boolean hasValidMoves() {
+    for (int x = 0; x < board.length; x++) {
+      for (int y = 0; y < board[x].length; y++) {
+        int[] pos = {x, y};
+
+        if (isValidMove(pos, turn)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private void increaseTurn() {
+    turn = (turn + 1) % playerCount;
+
+    if (turn == 0) {
+      turn = playerCount;
+    }
+  }
+
+  /**
+   * A way to emit data after the user attempts to place a tile
+   */
+  public class State {
+    /**
+     * A list of coordinates of all changed tiles
+     */
+    public ArrayList<int[]> updates = null;
+    /**
+     * Whether or not the attempted move was valid
+     */
+    public boolean isValidMove = false;
+    /**
+     * Whether or not the player was skipped since they had not any valid moves
+     */
+    public boolean isSkipped = false;
+
+    public State() {}
+
+    // Just for debugging
+    public String toString() {
+      String string =
+        "isValidMove: " + isValidMove + "\n"
+        + "isSkipped: " + isSkipped;
+
+      if (updates != null) {
+        string += "\nupdates";
+
+        for (int i = 0; i < updates.size(); i++) {
+          int[] update = updates.get(i);
+          string += "\n\t(" + update[0] + ", " + update[1] + ")";
+        }
+      }
+
+      return string;
+    }
+
   }
 }
