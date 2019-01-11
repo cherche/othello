20c5d7566f6403bcae53171088e5b2a142071fb9
Ryan Nguyen <contactryannguyen@gmail.com>
2019-01-10 20:39:17 -0500

    Completed main menu

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.font.*;
 import java.awt.event.*;
@@ -8,7 +9,7 @@ import java.util.*;
  * A virtual Reversi, with some options
  *
  * @author  Ryan Nguyen
- * @version 2019-01-07
+ * @version 2019-01-10
  */
 public class Game extends JPanel implements ActionListener {
   private static int width = 8;
@@ -16,27 +17,30 @@ public class Game extends JPanel implements ActionListener {
   private static int playerCount = 2;
   private static Color FORE = new Color(125, 130, 142);
   private static Color BACK = new Color(50, 54, 62);
-  private static Color SIDEBAR_BACK = new Color(40, 44, 53);
+  private static Color FORWARD_BACK = new Color(40, 44, 53);
   private static Color BOARD_BACK = new Color(0, 187, 84);
   private static Color BOARD_OUTLINE = BACK;
   private static JPanel main;
   private static CardLayout mainLayout;
+  private static JTextField playerCountField;
+  private static JButton play;
+  private static JPanel board = new JPanel();
   private static JComponent[] boardSidebarComponents;
 
   public static void main(String[] args) {
     JPanel content = new Game();
     // We want to design around a 6:5 viewport
     content.setPreferredSize(new Dimension(600, 500));
-    JFrame window = new JFrame("Othello");
-    window.setContentPane(content);
+    JFrame frame = new JFrame("Othello");
+    frame.setContentPane(content);
     // Setting the window size directly actually includes the title bar
     // in the height, which means that a window size of (600, 500)
     // would leave maybe (600, 480) for our actual content
-    window.pack();
-    window.setResizable(false);
+    frame.pack();
+    frame.setResizable(false);
     // Centres the window on the screen
-    window.setLocationRelativeTo(null);
-    window.setVisible(true);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
   }
 
   public Game() {
@@ -63,6 +67,8 @@ public class Game extends JPanel implements ActionListener {
     button.setIcon(createImageIcon(iconURL));
     button.setActionCommand(actionCommand);
     button.addActionListener(actionListener);
+    button.setOpaque(true);
+    button.setBorderPainted(false);
     return button;
   }
 
@@ -70,7 +76,6 @@ public class Game extends JPanel implements ActionListener {
     String iconURL = "icons/sidebar/" + name + ".png";
     JButton button = createIconButton(iconURL, actionCommand, actionListener);
     button.setOpaque(false);
-    button.setBorderPainted(false);
     return button;
   }
 
@@ -82,7 +87,7 @@ public class Game extends JPanel implements ActionListener {
 
   private JPanel initSidebar() {
     JPanel sidebar = new JPanel(new GridLayout(5, 0));
-    sidebar.setBackground(SIDEBAR_BACK);
+    sidebar.setBackground(FORWARD_BACK);
     sidebar.setPreferredSize(new Dimension(100, 500));
     JButton home = createSidebarButton("home", "home", this);
     sidebar.add(home);
@@ -107,41 +112,75 @@ public class Game extends JPanel implements ActionListener {
   }
 
   private JPanel initMenu() {
-    JPanel menu = new JPanel();
-    menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
+    JPanel menu = new JPanel(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+    JPanel container = new JPanel();
+    container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
     JLabel title = new JLabel("Othello");
     title.setAlignmentX(Component.CENTER_ALIGNMENT);
-    title.setFont(new Font("Nunito", Font.PLAIN, 45));
+    title.setFont(new Font("Gill Sans", Font.PLAIN, 52));
     title.setForeground(FORE);
-    menu.add(title);
-    /*
-    // In fact, we could probably generalize these radio buttons
-    // with a fancy class or something like that
-    JPanel colours = new JPanel();
-    ButtonGroup coloursGroup = new ButtonGroup();
-    colours.setAlignmentX(Component.CENTER_ALIGNMENT);
-    JRadioButton black = new JRadioButton("black");
-    black.setSelected(true);
-    coloursGroup.add(black);
-    colours.add(black);
-    JRadioButton white = new JRadioButton("white");
-    coloursGroup.add(white);
-    colours.add(white);
-    menu.add(colours);
-    */
-    JButton play = new JButton("Play");
-    play.setActionCommand("board");
-    play.addActionListener(this);
+    title.setBorder(new EmptyBorder(0, 0, 10, 0));
+    container.add(title);
+    // I just moved this to a new method because it was
+    // a huge pain to read right here. This is for readability.
+    // Actually, all the init methods are for readability and maintainability.
+    // They don't actually generalize anything, unlike the create methods.
+    JPanel configField = initPlayerCountField();
+    container.add(configField);
+    play = createIconButton("icons/menu/play.png", "play", this);
+    play.setBorder(new EmptyBorder(30, 0, 0, 0));
+    play.setOpaque(false);
     play.setAlignmentX(Component.CENTER_ALIGNMENT);
-    menu.add(play);
+    container.add(play);
+    container.setOpaque(false);
+    menu.add(container, c);
     menu.setOpaque(false);
     return menu;
   }
 
+  private JPanel initPlayerCountField() {
+    JPanel pair = new JPanel();
+    JLabel label = new JLabel(createImageIcon("icons/menu/player-count.png"));
+    label.setBorder(new EmptyBorder(0, 10, 0, 5));
+    /*
+    label.setFont(new Font("Open Sans", Font.PLAIN, 36));
+    label.setForeground(FORE);
+    */
+    pair.add(label);
+    JTextField field = new JTextField("2", 2);
+    field.setFont(new Font("Open Sans", Font.PLAIN, 36));
+    field.setForeground(FORE);
+    field.setOpaque(false);
+    field.setHorizontalAlignment(JTextField.CENTER);
+    field.setBorder(BorderFactory.createEmptyBorder());
+    field.addKeyListener(new KeyListener() {
+      public void keyTyped(KeyEvent e) {}
+
+      public void keyPressed(KeyEvent e) {}
+
+      public void keyReleased(KeyEvent e) {
+        int value = getPlayerCountFieldValue();
+
+        if (value == -1) {
+          play.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+          play.setIcon(createImageIcon("icons/menu/play-disabled.png"));
+        } else {
+          play.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+          play.setIcon(createImageIcon("icons/menu/play.png"));
+        }
+      }
+    });
+    playerCountField = field;
+    pair.add(field);
+    pair.setBackground(FORWARD_BACK);
+    pair.setAlignmentX(Component.CENTER_ALIGNMENT);
+    return pair;
+  }
   private static JPanel initBoardContainer() {
     // GridBagLayout vertically and horizontally centres its children by default
     JPanel boardContainer = new JPanel(new GridBagLayout());
-    JPanel board = initBoard();
+    JPanel board = createBoard();
     // Here, we'll do some calculations to figure out
     // how big to make the board
     board.setPreferredSize(new Dimension(480, 480));
@@ -151,9 +190,9 @@ public class Game extends JPanel implements ActionListener {
     return boardContainer;
   }
 
-  private static JPanel initBoard() {
-    // We may later use removeAll() if the user changes the configuration
-    JPanel board = new JPanel(new GridLayout(height, width));
+  private static JPanel createBoard() {
+    board.removeAll();
+    board.setLayout(new GridLayout(height, width));
 
     // We must iterate in this order because visually,
     // the components are added to a GridLayout left-to-right, top-to-bottom
@@ -174,9 +213,24 @@ public class Game extends JPanel implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     String actionCommand = e.getActionCommand();
 
-    if ("board".equals(actionCommand)) {
+    if ("play".equals(actionCommand)) {
+      int value = getPlayerCountFieldValue();
+
+      if (value == -1) {
+        play.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+        play.setIcon(createImageIcon("icons/menu/play-disabled.png"));
+        return;
+      } else {
+        play.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        play.setIcon(createImageIcon("icons/menu/play.png"));
+        playerCount = value;
+      }
+
       setSidebarMode(true);
       mainLayout.show(main, "boardContainer");
+      // Technically speaking, it's pretty inefficient to rebuild the board
+      // every time, but I'll leave efficiency for a later date
+      createBoard();
     } else if ("home".equals(actionCommand)) {
       /* Something like this to make sure they don't lose everything
       Object[] options = { "Continue", "Cancel" };
@@ -184,6 +238,21 @@ public class Game extends JPanel implements ActionListener {
       */
       setSidebarMode(false);
       mainLayout.show(main, "menu");
+    }
+  }
+
+  private static int getPlayerCountFieldValue() {
+    // Returns -1 if the playerCount would be unacceptable
+    try {
+      int value = Integer.parseInt(playerCountField.getText());
+
+      if (2 <= value && value <= 4) {
+        return value;
+      } else {
+        return -1;
+      }
+    } catch (Exception e) {
+      return -1;
     }
   }
 
